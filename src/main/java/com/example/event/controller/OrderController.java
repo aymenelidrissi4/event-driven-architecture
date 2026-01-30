d0708bf5f56559e08dafe9ea40091a48ad1ac05a
aelidrissimoubtassim <aelidrissimoubtassim@sqli.com>
2026-01-30 15:01:03 +0100

    test get mapping on tomcat

--- src/main/java/com/example/event/controller/OrderController.java
+++ src/main/java/com/example/event/controller/OrderController.java
@@ -1,6 +1,7 @@
 package com.example.event.controller;
 
 import com.example.event.service.OrderService;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -19,5 +20,10 @@ public class OrderController {
     public String placeOrder() {
         orderService.placeOrder();
         return "Order placed";
+    }
+
+    @GetMapping("/hello")
+    public String test(){
+        return "hello";
     }
 }
