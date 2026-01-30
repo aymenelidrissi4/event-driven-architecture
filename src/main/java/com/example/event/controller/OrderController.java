0ef770bcdfab705bc6f30c20f989fa1b8492f6dd
aelidrissimoubtassim <aelidrissimoubtassim@sqli.com>
2026-01-30 15:19:33 +0100

    Revert "test get mapping on tomcat"
    This reverts commit d0708bf5f56559e08dafe9ea40091a48ad1ac05a.

--- src/main/java/com/example/event/controller/OrderController.java
+++ src/main/java/com/example/event/controller/OrderController.java
@@ -1,7 +1,6 @@
 package com.example.event.controller;
 
 import com.example.event.service.OrderService;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -20,10 +19,5 @@ public class OrderController {
     public String placeOrder() {
         orderService.placeOrder();
         return "Order placed";
-    }
-
-    @GetMapping("/hello")
-    public String test(){
-        return "hello";
     }
 }
